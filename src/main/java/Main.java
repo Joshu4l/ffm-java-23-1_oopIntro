798966aceea0ba4d311fc4bd447e6611d694c146
joshuaalbert <albertjoshua5@gmail.com>
2023-08-29 12:26:22 +0200

    refactored the Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Instantiate a car
+        // Instantiate a first car
         Car my1stCar = new Car("Fiat", "Multipla", "Yellow", 2002);
-
         // Invoke its methods
         my1stCar.startCar();
         my1stCar.accelerate(30);
 
-        System.out.println(Car.allCars);
+        System.out.println("\n");
+
+        // Instantiate a second car
+        Car my2ndCar = new Car("BMW", "M4", "Grey", 2019);
+        // Invoke its methods
+        my2ndCar.startCar();
+        my2ndCar.accelerate(40);
+
+        // Show all cars that are existing so far:
+        System.out.println("\nExisting car objects so far:\n" + Car.allCars);
 
     }
 
